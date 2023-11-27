c97d6fcb0973f0353c96cbe5fd4d84226dd2c1f3
Luan Menezes <luanmenezesmluiz@gmail.com>
2023-11-27 12:41:19 -0300

    chore: arrumado o problema do jwt e finalizado o arquivo do middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
 import * as jose from 'jose';
+import supabase from '@/app/api/supabase';
 
 export default async function middleware(request: NextRequest) {
+  const errorMessages = {
+    invalidAdminToken: 'Invalid admin token',
+    adminTokenNotDefined: 'Admin token not defined',
+    bearerTokenNotDefined: 'Bearer token not defined',
+    tokenNotDefined: 'Token not defined',
+    emailNotDefined: 'Email not defined',
+    userNotFound: 'User not found',
+    signatureVerificationFailed: 'Signature verification failed',
+    unauthorizedRequest: 'Unauthorized request',
+  }
+
   if (request.nextUrl.pathname.startsWith('/api')) {
     if (request.nextUrl.pathname.startsWith('/api/auth')) {
       const adminToken = request.headers.get('admin-token') as string;
@@ -10,7 +22,7 @@ export default async function middleware(request: NextRequest) {
         if (adminToken !== process.env.APP_ADMIN_TOKEN) {
           return NextResponse.json(
             {
-              errorMessage: 'Invalid admin token',
+              errorMessage: errorMessages.invalidAdminToken,
             },
             { status: 401 }
           );
@@ -18,7 +30,7 @@ export default async function middleware(request: NextRequest) {
       } else {
         return NextResponse.json(
           {
-            errorMessage: 'Admin token not defined',
+            errorMessage: errorMessages.adminTokenNotDefined,
           },
           { status: 401 }
         );
@@ -29,7 +41,7 @@ export default async function middleware(request: NextRequest) {
       if (!bearerToken) {
         return NextResponse.json(
           {
-            errorMessage: 'Bearer token not defined',
+            errorMessage: errorMessages.bearerTokenNotDefined,
           },
           { status: 401 }
         );
@@ -40,7 +52,7 @@ export default async function middleware(request: NextRequest) {
       if (!token) {
         return NextResponse.json(
           {
-            errorMessage: 'Token not defined',
+            errorMessage: errorMessages.tokenNotDefined,
           },
           { status: 401 }
         );
@@ -49,16 +61,50 @@ export default async function middleware(request: NextRequest) {
       const signature = new TextEncoder().encode(process.env.JWT_SECRET);
 
       try {
-        console.log('token', token);
-        console.log('signature', signature);
-
         const jwt = await jose.jwtVerify(token, signature);
 
-        console.log('jwt', jwt);
-      } catch (error) {
+        if (jwt) {
+          const email = jwt.payload.email;
+
+          if (!email) {
+            return NextResponse.json(
+              {
+                errorMessage: errorMessages.emailNotDefined,
+              },
+              { status: 401 }
+            );
+          }
+
+          const user = await supabase
+            .from('apiauth')
+            .select('id, email')
+            .eq('email', email);
+
+          if (user.data && user.data.length > 0) {
+            return NextResponse.next();
+          } else {
+            return NextResponse.json(
+              {
+                errorMessage: errorMessages.userNotFound,
+              },
+              { status: 401 }
+            );
+          }
+        }
+      } catch (error: any) {
+        // Verificar se o erro é de assinatura inválida
+        if (error.message === 'signature verification failed') {
+          return NextResponse.json(
+            {
+              errorMessage: errorMessages.signatureVerificationFailed,
+            },
+            { status: 401 }
+          );
+        }
+
         return NextResponse.json(
           {
-            errorMessage: 'Unauthorized request',
+            errorMessage: errorMessages.unauthorizedRequest,
           },
           { status: 401 }
         );
